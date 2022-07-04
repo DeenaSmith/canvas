08edbaaa389c78d284a689615952066a8f9e80ed
Enrique Jimenez <ejimenez2292@gmail.com>
2022-07-04 14:11:07 -0500

    added template for reviews

---
+++ server/models/Review.js
@@ -0,0 +1,27 @@
+const {Schema, model} = require('mongoose')
+
+const reviewSchema = new Schema(
+    {
+        reviewText: {
+            type: String,
+            required: "Please let us know about your experience",
+            minlength: 1,
+            maxlength: 300
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        toJSON: {
+            getters: true
+        }
+    }
+)
+
+const Reviews = model('Reviews', reviewSchema)
+
+module.exports = Reviews
