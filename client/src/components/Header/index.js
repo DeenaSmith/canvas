8620bf82df09d48163fb6597464a604ba00c5c77
Samantha Wolff <samanthawolff07@gmail.com>
2022-07-06 22:45:49 -0500

    basic header layout

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <section>
+            <header className="canvas-header">
+                <h1>Canvas</h1>
+                <button>Login</button>
+                <button>Sign Up</button>
+            </header>
+        </section>
+    );
+}
+
+export default Header;
