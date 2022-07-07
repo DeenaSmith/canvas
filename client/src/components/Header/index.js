ee3c2822c5796e1663a44578970424b8a9ff528d
Samantha Wolff <samanthawolff07@gmail.com>
2022-07-06 23:28:57 -0500

    begin sidebar layout

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import Sidebar from '../Sidebar';
 
 
 function Header() {
     return (
         <section>
             <header className="canvas-header">
-                <h1>Canvas</h1>
-                <button>Login</button>
-                <button>Sign Up</button>
+                <div>
+                    <Sidebar> </Sidebar>
+                </div>
+                <div>
+                    <h1 className="header-h1">Canvas</h1>
+                </div>
+                <div className="login-button">
+                    <button>Login</button>
+                    <button>Sign Up</button>
+                </div>
             </header>
         </section>
     );
---
+++ client/src/components/Sidebar/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+
+
+function Sidebar() {
+    return (
+        <Menu>
+            <a className="menu-item" href="/">
+                Home
+            </a>
+
+            <a className="menu-item" href="#profile">
+                View Profile
+            </a>            
+        </Menu>
+    );
+};
+
+export default Sidebar;
