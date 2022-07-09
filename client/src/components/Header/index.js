b228a3b3020b55204d6dcbed8d8a31dce1c55d13
Deena Smith <deenasmith1630@gmail.com>
2022-07-09 16:11:44 -0500

    another conflict

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Sidebar from '../Sidebar';
-
-
 function Header() {
     return (
         <section>
@@ -9,22 +7,11 @@ function Header() {
                 <Sidebar> </Sidebar>
                 <h1>CANVAS</h1>
                 <div className="header-links">
-<<<<<<< HEAD
-<<<<<<< HEAD
-                    <a href="#login">Login  </a>
-                    <a href="signup">Sign-Up</a>
-=======
                     <a href="/login">Login  </a>
                     <a href="/signup">Sign-Up</a>
->>>>>>> feature/signup
-=======
-                    <a href="/login">Login  </a>
-                    <a href="/signup">Sign-Up</a>
->>>>>>> 851013fa82a94708a8ad59f2eff38d136544e76d
                 </div>
             </header>
         </section>
     );
 }
-
 export default Header;
