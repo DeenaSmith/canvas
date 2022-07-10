2b00b5954dd584a1f31162b6c7258253b420adb6
Enrique Jimenez <ejimenez2292@gmail.com>
2022-07-10 17:32:26 -0500

    working on tattoo gallery

--- client/src/pages/uploadPhoto.js
+++ client/src/pages/uploadPhoto.js
@@ -1,33 +1,16 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Dropzone from 'react-dropzone-uploader';
-import Modal from 'react-modal'
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+//import Auth from '../utils/auth';
+//import { useMutation } from '@apollo/client';
+//import { UPLOAD_FILE } from '../utils/mutations';
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
+
 
 const Upload = () => {
-  let subtitle;
-  const [modalIsOpen, setIsOpen] = React.useState(false)
-
-  function openModal() {
-    setIsOpen(true)
-  }
-  
-  function afterOpenModal() {
-    subtitle.style.color = '#f00'
-  }
-
-  function closeModal() {
-    setIsOpen(false)
-  }
+  const [img, setImg] = useState("http://placehold.it/180")
 
   const getUploadParams = () => {
     return { url: 'https://httpbin.org/post' }
@@ -39,30 +22,48 @@ const Upload = () => {
 
   const handleSubmit = (files, allFiles) => {
     console.log(files.map(f => f.meta))
-    allFiles.forEach(f => f.remove())
-  }
+    const reader = new FileReader()
+    reader.onload = e => {
+      setImg(e.target.result)
+    }
+    reader.readAsDataURL(files[0].file)
+    //allFiles.forEach(f => f.remove())
+  };
 
   return (
     <>
-    <div>
-      <button onClick={openModal}>Show us your work!</button>
-      <Modal
-      isOpen={modalIsOpen}
-      onAfterOpen={afterOpenModal}
-      onRequestClose={closeModal}
-      style={customStyles}
+      <Box
+        sx={{
+          minWidth: "auto",
+          maxWidth: "300px",
+          height:'500px',
+          margin: "0 auto",
+          float: "none",
+          marginBottom: "10px",
+          marginTop: "30px",
+        }}
       >
-          <button onClick={closeModal}>Close</button>
-    <Dropzone
-      getUploadParams={getUploadParams}
-      onChangeStatus={handleChangeStatus}
-      onSubmit={handleSubmit}
-      styles={{ dropzone: { minHeight: 150, maxHeight: 150 } }}
-    />
-    </Modal>
-    </div>
+        <Card variant="outlined">
+          <CardContent>
+            <div>
+              <Dropzone
+              inputContent={"Upload files or take photos"}
+              inputWithFilesContent={"Add more"}
+              submitButtonContent={"Save"}
+                getUploadParams={getUploadParams}
+                onChangeStatus={handleChangeStatus}
+                onSubmit={handleSubmit}
+                styles={{ dropzone: { minHeight: 150, maxHeight: 150 } }}
+              />
+            </div>
+            <div>
+             <img id='image' src={img} alt='preview'/>
+            </div>
+          </CardContent>
+        </Card>
+      </Box>
     </>
-  )
+  );
 }
 
 export default Upload;
