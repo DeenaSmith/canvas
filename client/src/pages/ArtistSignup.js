f4cd08e9818c4ae13e9558b542bea3c7f018b143
Samantha Wolff <samanthawolff07@gmail.com>
2022-07-09 22:01:40 -0500

    realized i havent been committing anything lol beggining sign up style

--- client/src/pages/ArtistSignup.js
+++ client/src/pages/ArtistSignup.js
@@ -3,7 +3,6 @@ import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
-
 const ArtistSignup = () => {
     const [formState, setFormState] = useState({
         username: '',
@@ -37,49 +36,54 @@ const ArtistSignup = () => {
         });
     };
 
-
     return (
 
-        <div className="container my-1">
+        <div className="container signup-form mt-5">
             {/* <Link to="/login">← Go to Login</Link> */}
+            <div className="row">
+                <div className="col-8">
+                    <h2>Signup</h2>
+                    <form onSubmit={handleFormSubmit} className="col-8">
+                        <div className="">
+                            <label htmlFor="username">User Name:</label>
+                            <input
+                                placeholder="First"
+                                name="username"
+                                type="username"
+                                id="username"
+                                onChange={handleChange}
+                            />
+                        </div>
 
-            <h2>Signup</h2>
-            <form onSubmit={handleFormSubmit}>
-                <div className="flex-row space-between my-2">
-                    <label htmlFor="username">User Name:</label>
-                    <input
-                        placeholder="First"
-                        name="username"
-                        type="username"
-                        id="username"
-                        onChange={handleChange}
-                    />
-                </div>
+                        <div className="">
+                            <label htmlFor="email">Email:</label>
+                            <input
+                                placeholder="youremail@test.com"
+                                name="email"
+                                type="email"
+                                id="email"
+                                onChange={handleChange}
+                            />
+                        </div>
 
-                <div className="flex-row space-between my-2">
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        placeholder="youremail@test.com"
-                        name="email"
-                        type="email"
-                        id="email"
-                        onChange={handleChange}
-                    />
+                        <div className="">
+                            <label htmlFor="pwd">Password:</label>
+                            <input
+                                placeholder="******"
+                                name="password"
+                                type="password"
+                                id="pwd"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="">
+                            <button type="submit">Submit</button>
+                        </div>
+                    </form>
                 </div>
-                <div className="flex-row space-between my-2">
-                    <label htmlFor="pwd">Password:</label>
-                    <input
-                        placeholder="******"
-                        name="password"
-                        type="password"
-                        id="pwd"
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="flex-row flex-end">
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
+                <img alt="red background" className="col-4 signup-img p-0" src="https://i.postimg.cc/ncN4LsQF/Screen-Shot-2022-07-09-at-9-17-46-PM.jpg"></img>
+                <img alt="flower tattoo" className="col-4 signup-img-2 p-0" src ="https://i.pinimg.com/originals/e8/0c/01/e80c01003d7bb9ef0357dd053135cded.png"></img>
+            </div>
             {error && <div>Sign up failed</div>}
         </div>
 
