54fc0a8fd6293d452b1f4f74967772861490b538
Samantha Wolff <samanthawolff07@gmail.com>
2022-07-11 18:52:32 -0500

    delete node_modules

--- client/src/pages/ArtistSignup.js
+++ client/src/pages/ArtistSignup.js
@@ -8,7 +8,6 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 
-
 const ArtistSignup = () => {
 
     const [formState, setFormState] = useState({
@@ -43,7 +42,6 @@ const ArtistSignup = () => {
         });
     };
 
-
     return (
         <>
         <Box sx={{ minWidth: 'auto', maxWidth:'300px', margin:'0 auto', float: 'none', marginBottom:'10px', marginTop: '30px' }}>
@@ -51,46 +49,53 @@ const ArtistSignup = () => {
         <CardContent>
         <div className="container my-1">
             {/* <Link to="/login">← Go to Login</Link> */}
+            <div className="row">
+                <div className="col-8">
+                    <h2>Signup</h2>
+                    <form onSubmit={handleFormSubmit} className="col-8">
+                        <div className="">
+                            <label htmlFor="username">User Name:</label>
+                            <input
+                                placeholder="First"
+                                name="username"
+                                type="username"
+                                id="username"
+                                onChange={handleChange}
+                            />
+                        </div>
 
-            <h2>Signup</h2>
-            <form onSubmit={handleFormSubmit}>
-                <div className="flex-row space-between my-2">
-                    <label htmlFor="username">User Name:</label>
-                    <input
-                        placeholder="First"
-                        name="username"
-                        type="username"
-                        id="username"
-                        onChange={handleChange}
-                    />
-                </div>
+                        <div className="">
+                            <label htmlFor="email">Email:</label>
+                            <input
+                                placeholder="youremail@test.com"
+                                name="email"
+                                type="email"
+                                id="email"
+                                onChange={handleChange}
+                            />
+                        </div>
 
-                <div className="flex-row space-between my-2">
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        placeholder="youremail@test.com"
-                        name="email"
-                        type="email"
-                        id="email"
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="flex-row space-between my-2">
-                    <label htmlFor="pwd">Password:</label>
-                    <input
-                        placeholder="******"
-                        name="password"
-                        type="password"
-                        id="pwd"
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="flex-row flex-end">
-                    <CardActions >
-                    <Button type="submit">Submit</Button>
-                    </CardActions>
-                </div>
-            </form>
+                        <div className="flex-row space-between my-2">
+                            <label htmlFor="pwd">Password:</label>
+                            <input
+                                placeholder="******"
+                                name="password"
+                                type="password"
+                                id="pwd"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        
+                        <div className="flex-row flex-end">
+                            <CardActions >
+                            <Button type="submit">Submit</Button>
+                            </CardActions>
+                        </div>
+                    </form>
+                <img alt="red background" className="col-4 signup-img p-0" src="https://i.postimg.cc/ncN4LsQF/Screen-Shot-2022-07-09-at-9-17-46-PM.jpg"></img>
+                <img alt="flower tattoo" className="col-4 signup-img-2 p-0" src ="https://i.pinimg.com/originals/e8/0c/01/e80c01003d7bb9ef0357dd053135cded.png"></img>
+            </div>
+        </div>
             {error && <div>Sign up failed</div>}
         </div>
         </CardContent>
