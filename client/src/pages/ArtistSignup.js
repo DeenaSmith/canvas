78ade9ac21023ca7384d7a24bd345190aaa54e55
Samantha Wolff <samanthawolff07@gmail.com>
2022-07-12 15:42:42 -0500

    copy styling for login page and clean up code

--- client/src/pages/ArtistSignup.js
+++ client/src/pages/ArtistSignup.js
@@ -2,10 +2,6 @@ import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
-import Box from '@mui/material/Box';
-/*import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent'; */
 import Button from '@mui/material/Button';
 
 const ArtistSignup = () => {
@@ -46,57 +42,55 @@ const ArtistSignup = () => {
         <div className="signup-pg">
             <div class="container">
                 <div class="row">
-                <div class="col-md-12 text-center">
-                    <h3 class="animate-charcter">CANVAS</h3>
-                </div>
+                    <div class="col-md-12 text-center">
+                        <h3 class="animate-charcter">CANVAS</h3>
+                    </div>
                 </div>
             </div>
-        <div className="container signup-card">
-        <Box sx={{ minWidth: 'auto', maxWidth:'300px', margin:'auto', float: 'none', marginBottom:'auto', marginTop: 'auto' }}>
-            <div>
-                <div className="col-12">
-                    <form onSubmit={handleFormSubmit} className="box">
-                    <h2 className="">Signup</h2>
-                    <p>Already have an account? <span><a href="/login" className='card-login'> Login</a></span></p>
-                        <div>
-                            <input
-                                placeholder="username"
-                                name="username"
-                                type="username"
-                                id="username"
-                                onChange={handleChange}
-                            />
-                        </div>
+            <div className="container signup-card">
+                <div>
+                    <div className="col-12">
+                        <form onSubmit={handleFormSubmit} className="box">
+                            <h2 className="">Signup</h2>
+                            <p>Already have an account? <span><a href="/login" className='card-login'> Login</a></span></p>
+                                <div>
+                                    <input
+                                    placeholder="username"
+                                    name="username"
+                                    type="username"
+                                    id="username"
+                                    onChange={handleChange}
+                                    />
+                                </div>
 
-                        <div>
-                            <input
-                                placeholder="youremail@email.com"
-                                name="email"
-                                type="email"
-                                id="email"
-                                onChange={handleChange}
-                            />
-                        </div>
+                                <div>
+                                    <input
+                                    placeholder="youremail@email.com"
+                                    name="email"
+                                    type="email"
+                                    id="email"
+                                    onChange={handleChange}
+                                    />
+                                </div>
 
-                        <div>
-                            <input
-                                placeholder="********"
-                                name="password"
-                                type="password"
-                                id="pwd"
-                                onChange={handleChange}
-                            />
-                        </div>
+                                <div>
+                                    <input
+                                    placeholder="********"
+                                    name="password"
+                                    type="password"
+                                    id="pwd"
+                                    onChange={handleChange}
+                                    />
+                                </div>
                         
-                        <div className="flex-row flex-end">
-                            <Button type="submit">Submit</Button>
-                        </div>
-                    </form>
+                                <div className="flex-row flex-end login-btn">
+                                    <Button type="submit">Submit</Button>
+                                </div>
+                                {error && <div>Sign up failed</div>}
+                        </form>
+                    </div>
                 </div>
-        </div>
-            {error && <div>Sign up failed</div>}
-        </Box>
-        </div>
+            </div>
         </div>
     );
 };
