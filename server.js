f11d2d24ec240def2997b89544aa574d3d4c3648
Samantha Wolff <samanthawolff07@gmail.com>
2022-07-11 22:10:15 -0500

    server issued for heroku deployment

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ if(process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"))
 };
 
-const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./server/schemas');
+const { authMiddleware } = require('./server/utils/auth');
+const db = require('./server/config/connection');
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
